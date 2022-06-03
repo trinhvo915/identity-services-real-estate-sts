eb8b5b013a893442877f55a23b798ed2c90a8535
vovantrinhdhbkdn@gmail.com <Trinh Vo>
2022-06-03 22:42:47 +0700

    init project identity services real estate system

---
+++ src/main/java/vn/identity/services/api/IdentityServicesRealEstateApplication.java
@@ -0,0 +1,20 @@
+package vn.identity.services.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+public class IdentityServicesRealEstateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityServicesRealEstateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/identity/services/api/IdentityServicesRealEstateStsApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.identity.services.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServicesRealEstateStsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
