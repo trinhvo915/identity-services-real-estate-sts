d96323d0667609adc7941984d103ef2a4b058928
TRINH VO VAN <Trinh Vo>
2022-06-04 13:15:59 +0700

    config connect to database sql server, config flyway and swagger with springdoc openapi ui

--- src/main/java/vn/identity/services/api/IdentityServicesRealEstateApplication.java
+++ src/main/java/vn/identity/services/api/IdentityServicesRealEstateApplication.java
@@ -2,15 +2,8 @@ package vn.identity.services.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class IdentityServicesRealEstateApplication {
 
     public static void main(String[] args) {
