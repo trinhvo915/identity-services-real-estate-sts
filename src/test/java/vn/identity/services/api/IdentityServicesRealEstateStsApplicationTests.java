fc851c3fa7c85182be353862ffe2c21991f91b3b
TRINH VO VAN <Trinh Vo>
2022-08-21 17:34:32 +0700

    edit function 0

--- src/test/java/vn/identity/services/api/IdentityServicesRealEstateStsApplicationTests.java
+++ src/test/java/vn/identity/services/api/IdentityServicesRealEstateStsApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class IdentityServicesRealEstateStsApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads1() {
     }
 
     @Test
