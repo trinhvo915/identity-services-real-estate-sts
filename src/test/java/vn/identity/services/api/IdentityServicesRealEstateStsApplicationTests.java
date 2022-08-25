5196f424ef9208454b02bad6f912c61c697fdcf5
TRINH VO VAN <Trinh Vo>
2022-08-25 23:02:46 +0700

    adbc

--- src/test/java/vn/identity/services/api/IdentityServicesRealEstateStsApplicationTests.java
+++ src/test/java/vn/identity/services/api/IdentityServicesRealEstateStsApplicationTests.java
@@ -27,18 +27,5 @@ class IdentityServicesRealEstateStsApplicationTests {
     // COMMIT 5.0
     // COMMIT 5.1
     // COMMIT 5.2
-<<<<<<< HEAD
-    // COMMIT 3.1
-    // COMMIT 3.2
-    // COMMIT 3.3
-    // COMMIT 3.4
-    // COMMIT 3.5
-    // COMMIT 3.6
-    // COMMIT 3.7
-    // COMMIT 3.8
-    // COMMIT 3.9
-    // COMMIT 4.0
-    // COMMIT 4.1
-    // COMMIT 4.2
 
 }
